290f03283f3f57f31e596032e728245d4f374b6f
Murs000 <vizortele096@gmail.com>
2024-04-26 16:13:43 +0400

    App. Configure Program StrartUp

--- PetApp/Program.cs
+++ PetApp/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using PetApp.Business.Mappers.Implementations;
+using PetApp.Business.Mappers.Interfaces;
+using PetApp.Business.Services.Implementations;
+using PetApp.Business.Services.Interfaces;
 using PetApp.Data.DataAccess.SqlServer;
+using PetApp.Data.DataAccess.SqlServer.Repositorys.Implementations;
+using PetApp.Data.DataAccess.SqlServer.Repositorys.Interfaces;
+using PetApp.Presentation.APIs.Implementations;
+using PetApp.Presentation.APIs.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +17,28 @@ var app = builder.Build();
 
 Configure(app);
 
+var apis = app.Services.GetServices<IApi>();
+foreach (var api in apis)
+{
+    if (api is null) throw new InvalidProgramException("Api not found");
+    api.Register(app);
+}
+
 app.Run();
 
 void RegisterServices(IServiceCollection services)
 {
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
     services.AddDbContext<PetAppDb>(options =>
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
     });
+    services.AddScoped<IPetPostRepository, PetPostRepository>();
+    services.AddScoped<IPetMapper, PetMapper>();
+    services.AddScoped<IPetService, PetService>();
+
+    services.AddTransient<IApi, PetApi>();
 }
 
 void Configure(WebApplication app)
@@ -27,7 +49,6 @@ void Configure(WebApplication app)
         app.UseSwaggerUI();
         using var scope = app.Services.CreateScope();
         var db = scope.ServiceProvider.GetRequiredService<PetAppDb>();
-        db.Database.EnsureCreated();
     }
 
     app.UseHttpsRedirection();
