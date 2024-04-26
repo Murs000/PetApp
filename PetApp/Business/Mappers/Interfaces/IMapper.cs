4048a92b9d5d9bab4419a567c0678adfb6ec5f8b
Murs000 <vizortele096@gmail.com>
2024-04-26 15:20:07 +0400

    App. Business. Add Mappers

---
+++ PetApp/Business/Mappers/Interfaces/IMapper.cs
@@ -0,0 +1,14 @@
+﻿using PetApp.Business.Models.Interfaces;
+using PetApp.Data.Entity.Interfaces;
+
+namespace PetApp.Business.Mappers.Interfaces
+{
+    public interface IMapper<TEntity, TModel> where TEntity : IEntity
+                                                 where TModel : IApiModel, new()
+    {
+        TModel Map(TEntity entity);
+        TEntity Map(TModel model);
+        List<TModel> Map(List<TEntity> entity);
+        List<TEntity> Map(List<TModel> model);
+    }
+}
---
+++ PetApp/Business/Mappers/Interfaces/IPetMapper.cs
@@ -0,0 +1,9 @@
+﻿using PetApp.Business.Models.Implementations;
+using PetApp.Data.Entity.Implementations;
+
+namespace PetApp.Business.Mappers.Interfaces
+{
+    public interface IPetMapper : IMapper<PetPost, PetModel>
+    {
+    }
+}
